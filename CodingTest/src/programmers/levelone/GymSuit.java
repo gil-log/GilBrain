c25c8668f0b132c5839c53daa24752acdbe65b0e
swgil007 <swgil007@naver.com>
2020-10-15 15:06:22 +0900

    체육복
    탐욕

---
+++ CodingTest/src/programmers/levelone/GymSuit.java
@@ -0,0 +1,41 @@
+package programmers.levelone;
+
+// https://programmers.co.kr/learn/courses/30/lessons/42862
+public class GymSuit {
+	
+	
+
+}
+
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n - lost.length;
+        
+        
+        
+        for(int i = 0; i < lost.length; i++){
+            for(int j = 0 ; j < reserve.length; j++){
+                if(lost[i]==reserve[j]){
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    answer++;
+                    break;
+                }
+            }
+        }
+        
+        for(int i = 0 ; i < reserve.length; i++){
+            for(int j = 0 ; j < lost.length; j++){
+                if(lost[j]==reserve[i]+1 || lost[j] == reserve[i]-1){
+                    answer++;
+                    lost[j] = -1;
+                    reserve[i] = -1;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
