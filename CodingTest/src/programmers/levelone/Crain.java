757873e4e010481916dd35da175f64c7e1b4a974
swgil007 <48559894+swgil007@users.noreply.github.com>
2020-10-15 20:55:27 +0900

    카카오 인형뽑기

---
+++ CodingTest/src/programmers/levelone/Crain.java
@@ -0,0 +1,58 @@
+package programmers.levelone;
+import java.util.*;
+
+//https://programmers.co.kr/learn/courses/30/lessons/64061
+public class Crain {
+
+}
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(0);
+        
+        //
+        
+        int load [] = new int [board.length];
+        
+        
+        
+        
+        //
+        
+        for(int j = 0 ; j < moves.length; j++){
+            for(int i = 0 ; i < board.length; i++){
+                
+                // 메모라이젼
+                if(load[moves[j]-1] > 0)
+                    i = load[moves[j]-1];
+                
+                else if(load[moves[j]-1] == -1)
+                    break;
+                
+                if(board[i][moves[j]-1] != 0){
+                    
+                    load[moves[j]-1] = i + 1;
+                    if(load[moves[j]-1] == board.length)
+                        load[moves[j]-1] = -1;
+                    
+                    if(stack.peek()!=board[i][moves[j]-1]){
+                        stack.push(board[i][moves[j]-1]);
+                        board[i][moves[j]-1] = 0 ;
+                        break;
+                    } else{
+                        stack.pop();
+                        board[i][moves[j]-1] = 0 ;
+                        answer+=2;
+                        break;
+                    }
+                    
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
