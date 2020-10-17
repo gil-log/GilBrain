ef4f2856cb4d2fdb3079d86de42417d0237f186a
swgil007 <48559894+swgil007@users.noreply.github.com>
2020-10-17 19:34:49 +0900

    완주하지못한선수

---
+++ CodingTest/src/programmers/levelone/NoCompletion.java
@@ -0,0 +1,30 @@
+package programmers.levelone;
+import java.util.*;
+
+//https://programmers.co.kr/learn/courses/30/lessons/42576
+public class NoCompletion {
+
+	
+}
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> hash = new HashMap<>();
+        
+        for(String a : participant){
+            hash.put(a, hash.getOrDefault(a, 0) + 1);
+        }
+        
+        for(String c : completion){
+            hash.put(c, hash.get(c) - 1);
+        }
+        for(String h : hash.keySet()){
+            if(hash.get(h) != 0){
+                return h;
+            }
+        }
+        
+        
+        return null;
+    }
+}
