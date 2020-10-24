bb36901546a335e6f0eaf1d85369fb791c24fc2f
swgil007 <48559894+swgil007@users.noreply.github.com>
2020-10-24 11:58:50 +0900

    문자열 내 P와 Y개수

---
+++ CodingTest/src/programmers/levelone/CountingPandY.java
@@ -0,0 +1,23 @@
+package programmers.levelone;
+//https://programmers.co.kr/learn/courses/30/lessons/12916
+class Solution {
+  boolean solution(String s) {
+      int sum = 0 ;
+      
+      for(int i = 0 ; i < s.length(); i ++){
+          char ch = s.charAt(i);
+          if(ch == 'p' || ch == 'P')
+              sum--;
+          else if(ch == 'y' || ch == 'Y')
+              sum++;
+      }
+      
+      if(sum==0)
+          return true;
+
+      return false;
+  }
+}
+public class CountingPandY {
+
+}
