cf7faf0318bb93969a80bdecefd6ade4718e00db
swgil007 <48559894+swgil007@users.noreply.github.com>
2020-10-25 07:34:33 +0900

    문자열 내림차순으로 배치하기

---
+++ CodingTest/src/programmers/levelone/MoonjaAscDesc.java
@@ -0,0 +1,24 @@
+package programmers.levelone;
+import java.util.Arrays;
+//https://programmers.co.kr/learn/courses/30/lessons/12917
+class Solution {
+  public String solution(String s) {
+      String answer = "";
+      
+      char[] charArray = new char[s.length()];
+      
+      for(int i = 0 ; i < s.length(); i++){
+          charArray[i] = s.charAt(i);
+      }
+      
+      Arrays.sort(charArray);
+      
+      for(int i = (s.length()-1) ; i > -1; i--){
+          answer+=charArray[i];
+      }
+      return answer;
+  }
+}
+public class MoonjaAscDesc {
+
+}
