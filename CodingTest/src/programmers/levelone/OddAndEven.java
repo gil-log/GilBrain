c18adf5a1139763418835a908292c5e7415b5a62
swgil007 <swgil007@naver.com>
2020-10-30 07:46:37 +0900

    짝수와 홀수

---
+++ CodingTest/src/programmers/levelone/OddAndEven.java
@@ -0,0 +1,16 @@
+package programmers.levelone;
+//https://programmers.co.kr/learn/courses/30/lessons/12937
+final class Solution {
+    public String solution(int num) {
+        String answer = "Odd";
+        
+        if(num % 2 == 0)
+            answer = "Even";
+        
+        return answer;
+    }
+}
+public class OddAndEven {
+
+    
+}
