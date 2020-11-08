a69fe6f355e8251a66ffa6a0720ac4bdb938f0b2
swgil007 <swgil007@naver.com>
2020-11-08 20:15:48 +0900

    직사각형 별찍기

---
+++ GilBrain/src/programmers/levelone/MakingStars.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//https://programmers.co.kr/learn/courses/30/lessons/12969
+final class Solution {
+
+    public static void solution(int a, int b) {
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        solution(a, b);
+
+    }
+}
+
+public class MakingStars {
+
+}
