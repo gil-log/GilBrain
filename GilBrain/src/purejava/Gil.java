718c4f911745f2de1cce0908ea02a81c4faed2bf
swgil007 <swgil007@naver.com>
2020-11-17 09:25:22 +0900

    LambdaInterface

---
+++ GilBrain/src/purejava/Gil.java
@@ -0,0 +1,13 @@
+package purejava;
+
+@FunctionalInterface
+interface Gil {
+    int log(int a, int b);
+}
+
+class Gillog {
+    public void logging(Gil gil) {
+        int number = gil.log(3, 4);
+        System.out.println("logging is " + number);
+    }
+}
