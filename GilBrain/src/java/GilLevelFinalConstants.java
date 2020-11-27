09084ab329ecf2d794536733da431b486880b781
swgil007 <swgil007@naver.com>
2020-11-27 09:47:35 +0900

    final class constants class 생성

---
+++ GilBrain/src/java/GilLevelFinalConstants.java
@@ -0,0 +1,10 @@
+package java;
+
+public final class GilLevelFinalConstants {
+    // 인스턴스화 방지
+    private GilLevelFinalConstants() {
+    }
+
+    public static final double LEVEL = 1;
+    public static final double GROWPOINT = 1.01;
+}
