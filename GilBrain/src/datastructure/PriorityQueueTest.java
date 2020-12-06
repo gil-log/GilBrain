5b6f5214235151526f4ead26170295c207838e64
swgil007 <swgil007@naver.com>
2020-12-06 09:47:44 +0900

    Priority Class Done

--- GilBrain/src/datastructure/PriorityQueueTest.java
+++ GilBrain/src/datastructure/PriorityQueueTest.java
@@ -28,6 +28,19 @@ public class PriorityQueueTest {
         for (int highest : priorityQueueHighest) {
             System.out.println("내림차순 우선순위 큐 : " + highest);
         }
+
+        PriorityQueue<Gillog> priorityQueue = new PriorityQueue<>();
+
+        priorityQueue.add(new Gillog(3650, "10년후 글"));
+        priorityQueue.add(new Gillog(31, "한달 후 글"));
+        priorityQueue.add(new Gillog(1, "첫번째 글"));
+        priorityQueue.add(new Gillog(365, "1년후 글"));
+
+        while (!priorityQueue.isEmpty()) {
+            Gillog gilLog = priorityQueue.poll();
+            System.out.println("글 넘버 : " + gilLog.getWriteRowNumber() + " 글 내용 : " + gilLog.getContent());
+        }
+
     }
 }
 
