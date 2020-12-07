07e306fa1f1b75fab2774ec365dcdb12a47e30e5
swgil007 <swgil007@naver.com>
2020-12-07 19:48:58 +0900

    Control Statement

---
+++ GilBrain/src/java/ControlStatement.java
@@ -0,0 +1,16 @@
+package java;
+
+public class ControlStatement {
+
+    public static void main(String[] args) {
+
+        int myAge = 26;
+
+        int otherGuyAge = 29;
+
+        if (myAge < otherGuyAge) {
+            System.out.println("형!");
+        }
+
+    }
+}
