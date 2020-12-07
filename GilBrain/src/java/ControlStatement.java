706c4034d26690b551233775d480c143a1940cee
swgil007 <swgil007@naver.com>
2020-12-07 20:18:33 +0900

    ControlStatement for

--- GilBrain/src/java/ControlStatement.java
+++ GilBrain/src/java/ControlStatement.java
@@ -12,6 +12,29 @@ public class ControlStatement {
             System.out.println("형!");
         }
 
+        if (myAge < otherGuyAge) {
+            System.out.println("형!");
+        } else if (myAge == otherGuyAge) {
+            System.out.println("친구야!");
+        } else {
+            System.out.println("야!");
+        }
+
+        switch (myAge) {
+            case 19:
+                System.out.println("고3");
+                break;
+            case 20:
+                System.out.println("대학생");
+                break;
+            case 21:
+                System.out.println("군대");
+                break;
+            default:
+                System.out.println("미설정");
+                break;
+        }
+
         for (int i = 0; i < 27; i++) {
             System.out.println(i);
         }
