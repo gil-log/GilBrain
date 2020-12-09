da4aa35bb07ba4ca2927b79e1ba6073b308f4e45
swgil007 <swgil007@naver.com>
2020-12-10 07:23:28 +0900

    Step One Dog

---
+++ GilBrain/src/baekjoon/stepone/Dog.java
@@ -0,0 +1,20 @@
+package baekjoon.stepone;
+
+//
+public class Dog {
+
+    public static void main(String[] args) {
+
+        String dogFirstLine = "|\\_/|";
+        String dogSecondLine = "|q p|   /}";
+        String dogThirdLine = "( 0 )\"\"\"\\";
+        String dogFourthLine = "|\"^\"`    |";
+        String dogFifthLine = "||_/=\\\\__|";
+
+        System.out.println(dogFirstLine);
+        System.out.println(dogSecondLine);
+        System.out.println(dogThirdLine);
+        System.out.println(dogFourthLine);
+        System.out.println(dogFifthLine);
+    }
+}
