5ae984ce472ca3c1edbc47730b791ecd3395a578
swgil007 <swgil007@naver.com>
2020-12-10 07:19:13 +0900

    Step One Cat

---
+++ GilBrain/src/baekjoon/stepone/Cat.java
@@ -0,0 +1,19 @@
+package baekjoon.stepone;
+
+//https://www.acmicpc.net/problem/10171
+public class Cat {
+
+    public static void main(String[] args) {
+
+        String catFirstLine = "\\    /\\";
+        String catSecondLine = " )  ( ')";
+        String catThirdLine = "(  /  )";
+        String catFourthLine = " \\(__)|";
+
+        System.out.println(catFirstLine);
+        System.out.println(catSecondLine);
+        System.out.println(catThirdLine);
+        System.out.println(catFourthLine);
+    }
+
+}
