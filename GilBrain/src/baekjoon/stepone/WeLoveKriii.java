d61dde0ee0312e4512296f77287cbfcf64531532
swgil007 <swgil007@naver.com>
2020-12-10 07:12:48 +0900

    StepOne WeLoveKriii

---
+++ GilBrain/src/baekjoon/stepone/WeLoveKriii.java
@@ -0,0 +1,11 @@
+package baekjoon.stepone;
+
+//https://www.acmicpc.net/problem/10718
+public class WeLoveKriii {
+
+    public static void main(String[] args) {
+        String message = "강한친구 대한육군";
+        System.out.println(message);
+        System.out.println(message);
+    }
+}
