8b18c6b9e7819a6940f41d5860aeb7124cef293c
swgil007 <swgil007@naver.com>
2020-12-10 07:09:47 +0900

    backjoon step one Hello World

---
+++ GilBrain/src/baekjoon/stepone/HelloWorld.java
@@ -0,0 +1,13 @@
+package baekjoon.stepone;
+
+//https://www.acmicpc.net/problem/2557
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        String answer = "Hello World!";
+
+        System.out.print(answer);
+    }
+
+}
