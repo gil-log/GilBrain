0bc81d3e8ba5bde9759cc642b8a846d1114ac4a3
GilLog <swgil007@naver.com>
2021-08-03 08:35:54 +0900

    Create JavaBeansPattern.java

---
+++ GilBrain/src/java/builder/JavaBeansPattern.java
@@ -0,0 +1,8 @@
+package gilbrain.builder;
+
+GillogCategory javaBeansPattern = new GillogCategory();
+javaBeansPattern.setLog("자바 빈 패턴");
+javaBeansPattern.setSubject("Java");
+javaBeansPattern.setViewFlag(1);
+javaBeansPattern.setMaxQuestion(2);
+javaBeansPattern.setCommentFlag(1);
