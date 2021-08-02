5fcda60a23d87f3968cb8e30280dc3a79d31ea3e
GilLog <swgil007@naver.com>
2021-08-03 08:36:27 +0900

    Update BuilderPattern.java

--- GilBrain/src/java/builder/BuilderPattern.java
+++ GilBrain/src/java/builder/BuilderPattern.java
@@ -1,27 +1,56 @@
 package gilbarin.builder;
 
-pulibc class Gillog {
-    // 필수 인자
+public Class GillogCategory {
     private final String log;
-    
-    // 선택 인자
     private final String subject;
-    private final String import;
-
-    // 필수 생성자
-    public Gillog(String log) {
-        this(log, "공통주제", "공통참조");
+    private final int viewFlag;
+    private final int commentFlag;
+    private final int maxQuestion;
+    
+    public static class Builder {
+    	// 필수 인자
+        private final String log;
+        private final String subject;
+        
+        // 선택 인자 기본값 초기화
+        private final int viewFlag = 0;
+        private final int commnetFlag = 0;
+        private final int maxQuestion = 0;
+        
+        public Builder(String log, String subject) {
+            this.log = log;
+            this.subject = subject;
+        }
+        
+        public Builder viewFlag(int val) {
+            viewFlag = val;
+            // 이렇게 return해주어 .으로 Chaining이 가능하다.
+            return this;
+        }
+        
+        public Builder commentFlag(int val) {
+            commentFlag = val;
+            return this;
+        }
+        
+        public Builder maxQuestion(int val) {
+            maxQuestion = val;
+            return this;
+        }
+        
+        public GillogCategory build() {
+            if(viewFlag > 1 || commentFlag > 1) {
+            	throw new IllealArgumentException("Flag 값 오인 사용");
+            }
+            return new Gillog(this);
+        }
     }
     
-    // 1개 선택 인자 포함 생성자
-    public Gillog(String log, String subject) {
-    	this(log, subject, "공통참조");
-    }
-    
-    // 모든 선택 인자까지 포함된 생성자
-    public Gillog(String log, String subject, String import) {
-        this.log = log;
-        this.subject = subject;
-        this.import = import;
+    private GillogCategory(Builder builder) {
+        log = builder.log;
+        subject = builder.subject;
+        viewFlag = builder.viewFlag;
+        commnetFlag = builder.commentFlag;
+        maxQuestion = builder.maxQuestion;
     }
 }
